48cea63c1374755724e692831fbb22fd06df1657
muzam2 <muzam2@yahoo.com>
2026-05-20 12:48:55 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,11 +3,9 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 
-
 android {
     namespace = "com.prayerwidget"
     compileSdk = 35
-
 
     defaultConfig {
         applicationId = "com.prayerwidget"
@@ -17,7 +15,6 @@ android {
         versionName = "3.2"
     }
 
-
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -25,18 +22,23 @@ android {
         }
     }
 
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-
     kotlinOptions {
         jvmTarget = "17"
     }
 
-
     buildFeatures {
         viewBinding = true
     }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.activity:activity-ktx:1.9.0")
+    implementation("com.google.android.material:material:1.12.0")
+}
